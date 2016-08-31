b3669b2704c0590bc9089f4f2a66da1ee365e090
kendallgoodwin <kendall@kendallgoodwin.com>
2016-08-31 16:59:17 -0700

    css modules working, sort of

--- yelp/webpack.config.js
+++ yelp/webpack.config.js
@@ -19,11 +19,48 @@ const isDev = NODE_ENV === 'development';
 
 // ...
 
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+      l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+config.module.loaders.push(newloader);
+cssloader.test =
+  new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+
 var config = getConfig({
 	isDev: isDev,
   in: join(__dirname, 'src/app.js'),
   out: join(__dirname, 'dist'),
   clearBeforeBuild: true
 })
+
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+])
 
 module.exports = config;
